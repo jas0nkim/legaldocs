3c3bbd9e39d693c31b35695461eb7dfdfbb639c2
Jason Kim <jason@realwebsolutions.com>
2014-10-27 15:09:11 -0400

    small update

--- officeengine/public_api/index.php
+++ officeengine/public_api/index.php
@@ -4,7 +4,7 @@ define('APP_PUBLIC_PATH', realpath(dirname(__FILE__)));
 define('APP_MASTER_PATH', realpath(APP_PUBLIC_PATH . '/../'));
 define('APP_VENDOR_PATH', realpath(APP_MASTER_PATH . '/vendor'));
 define('APP_VERSION', 'v1');
-define('URL_PREFIX', '/' . APP_VERSION);
+define('URL_PREFIX', '/api/' . APP_VERSION);
 
 require APP_VENDOR_PATH . '/autoload.php';
 
@@ -13,15 +13,16 @@ $app = new \Silex\Application();
 /**
  * example
  */
-$app->get(URL_PREFIX . '/hello/{name}', function ($name) use ($app) {
+$app->get(URL_PREFIX . '/hello/{name}', function (\Silex\Application $app, $name) {
     
     return 'Hello ' . $app->escape($name);
 });
 
-$app->get(URL_PREFIX . '/hahaha/{name}', function ($name) use ($app) {
+$app->get(URL_PREFIX . '/parse', function (\Silex\Application $app) {
     
     return 'Hello ' . $app->escape($name);
 });
+
 
 
 $app->run();
